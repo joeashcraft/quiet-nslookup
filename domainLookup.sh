fedd19b8aba2f9162aa5041062058a053ce45401
Joe Ashcraft <joeashcraft@gmail.com>
2012-04-05 01:34:54 -0500

    add script V1

---
+++ domainLookup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This should lookup the IP for a domain and return the first IP provided.
+
+ns=208.67.222.222
+#if [[ validateHost ]] ; then
+	hostQuery=$1
+#else
+#	usage
+#fi
+
+usage()
+{
+	cat<<EOF
+	host lookup -- Joe T. Ashcraft
+	
+	This script should return the first IP address of the domain provided. 
+	
+	usage:
+	$0 [optional nameserver] domain
+	
+	example:
+	$0 ns1.afraid.org h4ck.me
+EOF
+}
+
+findIP()
+{
+#        dig @$ns $hostQuery
+	host $hostQuery $ns |\
+	grep $hostQuery |\
+	awk '{print $4}'
+}
+
+# validateHost doesn't work yet
+validateHost()
+{
+	echo $1 |\
+	egrep "(?=^.{1,254}$)(^(?:(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)"
+}
+
+if [[ $1 != "" ]] ; then
+	findIP
+else
+	usage
+fi
+
+
